3df3b9ad344cd86cbb33e925f18058b65658456c
David-Kirsch <davidjkirsch@gmail.com>
2021-01-27 18:13:40 -0500

    Completed Set 2 problems

---
+++ Recursion Problems Set 2/isPalindrome.js
@@ -0,0 +1,20 @@
+//Write a recursive function called isPalindrome which returns true
+//if the string passed to it is a palindrome. Otherwise it returns
+//false.
+
+function isPalindrome(str) {
+  if (str.length === 1 || str.length === undefined) {
+    return true;
+  }
+  if (str[0] === str[str.length - 1]) {
+    return isPalindrome(str.slice(1, str.length - 1));
+  } else {
+    return false;
+  }
+}
+
+console.log(isPalindrome("awesome")); // false
+console.log(isPalindrome("foobar")); // false
+console.log(isPalindrome("tacocat")); // true
+console.log(isPalindrome("amanaplanacanalpanama")); // true
+console.log(isPalindrome("amanaplanacanalpandemonium")); // false
